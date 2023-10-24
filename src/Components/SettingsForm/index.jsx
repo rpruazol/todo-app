3326fc09153ec5dd84b75c2653506ba49721b4e1
rpruazol <rpruazol@gmail.com>
2023-10-24 00:38:58 -0400

    show settings made in settingsform

--- src/Components/SettingsForm/index.jsx
+++ src/Components/SettingsForm/index.jsx
@@ -6,47 +6,84 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select'; 
+import Select from '@mui/material/Select';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
-const SettingsForm = () => {
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+  const SettingsForm = () => {
   const settings = useContext(DisplayContext);
+  const [changes, setChanges] = useState({});
+
+  useEffect(() => {
+    // console.log(changes)
+  }, [changes])
 
   return (
-    <>
-      <FormGroup>
-        <FormControlLabel 
-        control={<Switch 
-          value={settings.hideCompleted}
-          onChange={() => {
-            let setting =  settings.hideCompleted === true ? false : true
-            settings.toggleHideCompleted(setting)
-            console.log(setting)
-            }}
-          checked={settings.hideCompleted}
-           />}
-          
-        label="Hide Completed Tasks"/>
-        
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Tasks Per Page</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={settings.page}
-            label="Tasks per page"
-            onChange={(e) => {
-              // console.log(e.target.value)
-              settings.changeTasksPerPage(e.target.value)
-              }
-            }
-          >
-            <MenuItem value={3}>3</MenuItem>
-            <MenuItem value={10}>10</MenuItem>
-            <MenuItem value={20}>20</MenuItem>
-          </Select>
-        </FormControl>
-      </FormGroup>
-    </>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid item xs={8} direction="row">
+        <Item>
+          <FormGroup>
+            <FormControlLabel
+              control={<Switch
+                value={settings.hideCompleted}
+                onChange={() => {
+                  let setting = settings.hideCompleted === true ? false : true
+                  settings.toggleHideCompleted(setting)
+                  setChanges({...changes, 'hideCompleted': setting}) //async, this
+                  console.log(changes)
+                }}
+
+                checked={settings.hideCompleted}
+              />}
+
+              label="Hide Completed Tasks" />
+
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Tasks Per Page</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={settings.display}
+                label="Tasks per page"
+                onChange={(e) => {
+                  settings.changeTasksPerPage(e.target.value)
+                  setChanges({...changes, 'display': e.target.value})
+                  console.log('changes', changes)
+
+                }
+                }
+              >
+                <MenuItem value={3}>3</MenuItem>
+                <MenuItem value={10}>10</MenuItem>
+                <MenuItem value={20}>20</MenuItem>
+              </Select>
+            </FormControl>
+          </FormGroup>
+        </Item>
+      </Grid>
+      <Grid item xs={4} direction="row">
+        <Item>
+          <h2>Changes made:</h2>
+                {Object.keys(changes).map((key, idx) => {
+                  console.log(changes)
+                  return (
+                    <p key={idx}>{`${key} : ${changes[key]}`}</p>
+                  )
+                })}
+        </Item>
+      </Grid>
+    </Box>
   )
 }
 
